77a5f50b1cead09051d4eb61e6b69ff930cdebfd
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-05-03 13:35:33 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function calc(a, b, op) {
+    if (op === "+") return a + b;
+    if (op === "-") return a - b;
+    if (op === "*") return a * b;
+    if (op === "/") return a / b;
+}
+
+console.log(calc(10, 5, "+"));
